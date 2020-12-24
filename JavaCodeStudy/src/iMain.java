1e8296a67b56f281b8d40f048aca521f20e0e5bc
BuildTools <unconfigured@null.spigotmc.org>
2020-12-24 23:11:53 +0700

    I add new message in System.out.print

--- JavaCodeStudy/src/iMain.java
+++ JavaCodeStudy/src/iMain.java
@@ -1,5 +1,5 @@
 public class iMain {
     public static void main(String[] args){
-        System.out.print("Hello World!");
+        System.out.print("Hello World! I change To Version 2");
     }
 }
