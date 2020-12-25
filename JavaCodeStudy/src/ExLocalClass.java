7741fe3d26541f75025b65936b00b2b2d8ae1b1f
khongpak <74703923+khongpak@users.noreply.github.com>
2020-12-25 11:26:01 +0700

    ได้เพิ่มคลาสซ้อนเข้าไปจึงเรียกผิด แต่ยังมีbugอยู่

--- JavaCodeStudy/src/ExLocalClass.java
+++ JavaCodeStudy/src/ExLocalClass.java
@@ -1,19 +1,22 @@
 // This Code is from http://bit.ly/3hhCDPB //
 public class ExLocalClass {
-    public class OuterClass{
         public void display(){
             int num = 23;
+
             class Inner{
                 public void printMe(){
                     System.out.println("This is method inner class " + num);
                 }
             }
+
             Inner Obj = new Inner();
             Obj.printMe();
         }
-        public static void main(String[] args){
-            OuterClass outer = new OuterClass();
+
+    public static void main(String[] args){
+            ExLocalClass outer = new ExLocalClass();
             outer.display();
         }
-    }
+
 }
+
