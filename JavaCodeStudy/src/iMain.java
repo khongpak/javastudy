d4de822e211aa7102143fa026b92d378f20372be
khongpak <afexim5@gmail.com>
2021-04-19 22:37:46 +0700

    iMain.java is my first file I created in Git.

--- JavaCodeStudy/src/iMain.java
+++ JavaCodeStudy/src/iMain.java
@@ -1,5 +1,17 @@
+import java.io.*;
 public class iMain {
-    public static void main(String[] args){
-        System.out.print("Hello World! I change To Version 2");
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("What's your name? ");
+        String name = reader.readLine();
+        System.out.print("What's your lastname? ");
+        String lastname = reader.readLine();
+        System.out.print("How old are you? ");
+        String age = reader.readLine();
+
+        int iAge = Integer.parseInt(age);
+
+        System.out.println("My name is "+name + " "+lastname+" My ages is "+ (iAge+1));
+
     }
 }
